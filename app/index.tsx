17ccc27b151001fcaa4b3a58c66e9ef3dd0f69df
Rodrigo <rodrigoludkeplay@gmail.com>
2025-08-18 14:39:28 -0300

    login (appMobile) - ControleFrotas

---
+++ app/index.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
+import { useRouter } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export default function LoginScreen() {
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const router = useRouter();
+
+    const handleLogin = async () => {
+        try {
+            const response = await fetch("http://192.168.1.160:3000/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, senha }),
+            });
+
+
+            const data = await response.json();
+
+            if (response.ok) {
+                // Salva token no AsyncStorage
+                await AsyncStorage.setItem("token", data.token);
+
+                Alert.alert("Sucesso", "Login realizado!");
+                router.replace("/(tabs)/Temp");
+            } else {
+                Alert.alert("Erro", data.error || "Falha no login");
+            }
+        } catch (error) {
+            Alert.alert("Erro", "Não foi possível conectar ao servidor");
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Login</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Senha"
+                secureTextEntry
+                value={senha}
+                onChangeText={setSenha}
+            />
+            <Button title="Entrar" onPress={handleLogin} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, justifyContent: "center", padding: 20 },
+    title: { fontSize: 24, marginBottom: 20, textAlign: "center" },
+    input: {
+        borderWidth: 1,
+        borderColor: "#ccc",
+        padding: 10,
+        marginBottom: 15,
+        borderRadius: 5,
+    },
+});
